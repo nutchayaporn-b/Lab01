de39e9868d8298ec92455b60ad8d75f993f8a074
ThisGameZR <timezazare@gmail.com>
2022-07-01 10:27:31 +0700

    5.4) The sizes of the socks are S, M, and L, add the code to show the size of the sock in the single line

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ const app = Vue.createApp({
       link: "https://www.camt.cmu.ac.th",
       inStock: true,
       onSale: true,
+      details: ["80% cotton", "20% polyester"],
+      variants: [
+        { id: 1, color: "green" },
+        { id: 2, color: "blue" },
+      ],
+      sizes: ["S", "M", "L"],
     };
   },
 });
