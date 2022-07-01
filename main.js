3774daaab3b76617878470856c5226751327185b
ThisGameZR <timezazare@gmail.com>
2022-07-01 10:21:53 +0700

    3.6) Add a new link to the product name, so that when we click on the product name it will lead the user to www.camt.cmu.ac.th

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       description: "A pair of warm, fuzzy socks",
+      image: "./assets/images/socks_green.jpg",
+      link: "https://www.camt.cmu.ac.th",
     };
   },
 });
